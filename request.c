3a096ccb5791dbcee4cec456c6d0eba394782233
rajsekharde <rajsekharde612@gmail.com>
2026-03-21 08:28:04 +0530

    Parse headers into key-value pairs

--- request.c
+++ request.c
@@ -43,28 +43,54 @@ int validate_request(http_request *req)
 
 int parse(char* buffer)
 {
-    char* method = buffer;
+    char* method = buffer; // first char of method
+    if(!method) return -1;
 
     char* path = strchr(method, ' ');
-    *path = '\0';
-    path += 1;
+    if (!path) return -1;
+    *path = '\0'; // terminate method at first space
+    path += 1; // first char of path
 
     char* version = strchr(path, ' ');
-    *version = '\0';
-    version += 1;
+    if (!version) return -1;
+    *version = '\0'; // terminate path at 2nd space
+    version += 1; // first char of version
 
     char* header = strstr(version, "\r\n");
-    *header = '\0';
-    header += 2;
+    if (!header) return -1;
+    *header = '\0'; // terminate version at 1st line break
+    header += 2; // headers start
 
     header_t headers[10];
+    int header_count = 0;
 
-    for(int i=0; i<10; i++)
+    while(header_count <= 10)
     {
         if(strstr(header, "\r\n") == header) break;
+        char* key = header;
+        // if (!key) return -1;
+
+        char* value = strstr(key, ": ");
+        // if (!value) return -1;
+        *value = '\0';
+        value += 2;
+
+        header = strstr(value, "\r\n");
+        if(!header) break;
+        *header = '\0';
+        header += 2;
+
+        header_t h = {key, value};
+        headers[header_count] = h;
+        header_count++;
     }
 
     printf("Method: %s\nPath: %s\nVersion: %s\n\n", method, path, version);
+    printf("Headers:\n");
+    for(int i=0; i<header_count; i++)
+    {
+        printf("Key: %s, Value: %s\n", headers[i].key, headers[i].value);
+    }
 
     return 0;
 }
